67792b66834c54dc1a5da5f27b76030e0086d34c
Ximena De Leon <deleon.xime@gmail.com>
2026-04-17 13:16:38 -0500

    remove all Asgardeo code from frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
 
-const config = {
-  signInRedirectURL: "https://lab-10-1-wi1f.onrender.com/",
-  signOutRedirectURL: "https://lab-10-1-wi1f.onrender.com/",
-  clientID: "7IM589V4c9vF96f_aDoGs46fLO8a",
-  baseUrl: "https://api.asgardeo.io/t/fullstackweb",
-  scope: ["openid", "profile"]
-};
-
 ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
 );
