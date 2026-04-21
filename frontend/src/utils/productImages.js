2976caa345c5ea48df389bbc5f624b01e6c97cab
Ximena De Leon <deleon.xime@gmail.com>
2026-04-21 11:59:45 -0500

    new product

--- frontend/src/utils/productImages.js
+++ frontend/src/utils/productImages.js
@@ -5,5 +5,7 @@ export function getLocalProductImageUrls(productId) {
     `/images/products/product-${productId}.jpg`,
     `/images/products/product-${productId}-2.jpg`,
     `/images/products/product-${productId}-3.jpg`,
+    `/images/products/product-${productId}-4.jpg`,
+
   ];
 }
